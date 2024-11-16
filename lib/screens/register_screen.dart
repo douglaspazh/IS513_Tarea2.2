e8dfd6f2a92f9ff60b4d10c7a9576eb26f647b83
Douglas Paz <douglas.paz@unah.hn>
2024-11-16 16:20:00 -0600

    Actualizado el formulario para registro con los widgets reutilizables
    Se ha cumplido con todos los requerimientos para la pantalla de registro.

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:is513_tarea2_2/widgets/clickable_text.dart';
+import 'package:is513_tarea2_2/widgets/custom_textfield.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -8,51 +10,154 @@ class RegisterScreen extends StatefulWidget {
 }
 
 class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _phoneController = TextEditingController();
   final _passwordController = TextEditingController();
   final _confirmPasswordController = TextEditingController();
 
+  // Variables regex para validar
+  final _emailRegex = RegExp(r'^[a-zA-Z._-]+[a-zA-Z]+@(unah\.edu\.hn|unah\.hn)$');
+  final _phoneRegex = RegExp(r'^[39]\d{7}$');
+  final _passwordRegex = RegExp(r'^(?=.*[A-Z])(?=.*[!@#$%^&*:;<>/-]).{8,}$');
+
+  void _goToLogin() {
+    Navigator.pushReplacementNamed(context, '/login');
+  }
+
+  void _register() {
+    if (_formKey.currentState!.validate()) {
+      print('Nombre: ${_nameController.text}');
+      print('Correo: ${_emailController.text}');
+      print('Teléfono: ${_phoneController.text}');
+      print('Contraseña: ${_passwordController.text}');
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('¡Registro exitoso!')),
+      );
+      _goToLogin();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Registro')),
+        title: const Text('Registrarse')),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: _nameController,
-            ),
-            TextField(
-              controller: _emailController,
-            ),
-            TextField(
-              controller: _phoneController,
-            ),
-            TextField(
-              controller: _passwordController,
-            ),
-            TextField(
-              controller: _confirmPasswordController,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              child: const Text('Registrarse')
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        child: Form(
+          key: _formKey,
+          child: SingleChildScrollView(
+            child: Column(
               children: [
-                const Text('Ya tienes una cuenta?'),
-                TextButton(
-                  onPressed: () {},
-                  child: const Text('Iniciar sesión'))
+                // Nombre
+                CustomTextField(
+                  controller: _nameController,
+                  labelText: 'Nombre',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'El nombre es obligatorio';
+                    } else if (value.length < 3 || value.length > 10) {
+                      return 'El nombre debe tener entre 3 y 10 caracteres';
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+            
+                const SizedBox(height: 14),
+                
+                // Correo
+                CustomTextField(
+                  controller: _emailController,
+                  labelText: 'Correo',
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'El correo es obligatorio';
+                    } else if (!_emailRegex.hasMatch(value)) {
+                      return 'El correo debe ser institucional (@unah.edu.hn o @unah.hn)';
+                    }
+                    return null;
+                  },
+                ),
+                
+                const SizedBox(height: 14),
+                
+                // Telefono
+                CustomTextField(
+                  controller: _phoneController,
+                  labelText: 'Telefono',
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'El telefono es obligatorio';
+                    } else if (!_phoneRegex.hasMatch(value)) {
+                      return 'El telefono debe iniciar con 3 u 8, y tener solo 8 digitos';
+                    }
+                    return null;
+                  },
+                ),
+                
+                const SizedBox(height: 14),
+                
+                // Constraseña
+                CustomTextField(
+                  controller: _passwordController,
+                  labelText: 'Contraseña',
+                  isPassword: true,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'La contraseña es obligatoria';
+                    } else if (value.length < 8) {
+                      return 'La contraseña debe tener al menos 8 caracteres';
+                    } else if (!_passwordRegex.hasMatch(value)) {
+                      return 'La contraseña debe tener al menos una mayuscula y un caracter especial';
+                    }
+                    return null;
+                  },
+                ),
+                
+                const SizedBox(height: 14),
+                
+                // Repetir contraseña
+                CustomTextField(
+                  controller: _confirmPasswordController,
+                  labelText: 'Confirmar contraseña',
+                  isPassword: true,
+                  validator: (value) {
+                    if (value != _passwordController.text) {
+                      return 'Las contraseñas no coinciden';
+                    }
+                    return null;
+                  },
+                ),
+                
+                const SizedBox(height: 14),
+                
+                // Boton registrarse
+                ElevatedButton(
+                  onPressed: _register,
+                  child: const Text('Registrarse')
+                ),
+                
+                const SizedBox(height: 8),
+            
+                // Opción para iniciar sesion
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text('¿Ya tienes una cuenta?'),
+                    const SizedBox(width: 4),
+                    ClickableText(
+                      text: 'Inicia sesión',
+                      onTap: _goToLogin
+                    )
+                  ],
+                )
               ],
-            )
-          ],
+            ),
+          ),
         ),
       ),
     );
